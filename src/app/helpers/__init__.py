168d6900637c3bd856254dbe2bc64b61a75611fc
AIFactory <aifactory@container>
2026-07-17 19:21:16 +0000

    aifactory: subtask-1-1 - Create src/app/helpers package with const_2.py module

---
+++ src/app/helpers/__init__.py
@@ -0,0 +1,3 @@
+"""Helper functions for aifactory-demo."""
+
+__version__ = "0.1.0"
---
+++ src/app/helpers/const_2.py
@@ -0,0 +1,6 @@
+"""Constant helper function returning 2."""
+
+
+def get_2():
+    """Return the integer 2."""
+    return 2
