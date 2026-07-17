9a8a3070ad264211d4f542f1db50fdd933a1a623
AIFactory <aifactory@container>
2026-07-17 22:03:00 +0000

    aifactory: subtask-1-1 - Create src/app/helpers/double.py with double(n) function

---
+++ src/app/helpers/double.py
@@ -0,0 +1,13 @@
+"""Double value helper function."""
+
+
+def double(n: int) -> int:
+    """Return the double of a number.
+
+    Args:
+        n: The number to double.
+
+    Returns:
+        The value of n multiplied by 2.
+    """
+    return n * 2
