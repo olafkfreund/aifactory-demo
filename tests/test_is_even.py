241091f45bdc5470056ca6bf67c93aeb18f3b4f6
AIFactory <aifactory@container>
2026-07-22 21:12:35 +0000

    aifactory: subtask-1-2 - Create pytest tests for is_even function

---
+++ tests/test_is_even.py
@@ -0,0 +1,21 @@
+from app.helpers.is_even import is_even
+
+
+def test_is_even_zero():
+    assert is_even(0) is True
+
+
+def test_is_even_positive():
+    assert is_even(4) is True
+
+
+def test_is_even_negative():
+    assert is_even(-2) is True
+
+
+def test_is_even_odd_positive():
+    assert is_even(3) is False
+
+
+def test_is_even_odd_negative():
+    assert is_even(-7) is False
